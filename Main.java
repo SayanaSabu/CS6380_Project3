99f1dec500991023618fae575f434272eba54367
sriharshav004 <snsh.harsha@gmail.com>
2023-05-01 00:54:58 -0500

    Establish handshake

--- Main.java
+++ Main.java
@@ -4,53 +4,42 @@ public class Main {
             int currNodeUID = Integer.parseInt(args[0]);
             Node currNode = ReadConfig.read(currNodeUID);
 
-            System.out.println("UID: " + currNode.getUID());
-            System.out.println("Hostname: " + currNode.getHostName());
-            System.out.println("Port: " + currNode.getPort());
+            TCPServer server = new TCPServer(currNode);
+            Runnable servRunnable = new Runnable() {
+                public void run() {
+                    server.startListening();
+                }
+            };
+            Thread servThread = new Thread(servRunnable);
+            servThread.start();
 
-            for (Node n : currNode.getNeighbourNodes()) {
-                System.out.println("Neighbour: " + n.getUID());
+            for (Node neighbourNode : currNode.getNeighbourNodes()) {
+                Runnable cliRunnable = new Runnable() {
+                    public void run() {
+                        try {
+                            Thread.sleep(5000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                        TCPClient client = new TCPClient(currNode, neighbourNode);
+                        client.connect();
+
+                        currNode.addNeighbourClient(client);
+                    }
+                };
+                Thread cliThread = new Thread(cliRunnable);
+                cliThread.start();
             }
 
-            // TCPServer server = new TCPServer(currNode);
-            // Runnable servRunnable = new Runnable() {
-            // public void run() {
-            // server.startListening();
-            // }
-            // };
-            // Thread servThread = new Thread(servRunnable);
-            // servThread.start();
-
-            // for (Node neighbourNode : currNode.getAllNodes()) {
-            // if (currNode.isNodeNeighbour(neighbourNode.getUID())) {
-            // Runnable cliRunnable = new Runnable() {
-            // public void run() {
-            // try {
-            // Thread.sleep(5000);
-            // } catch (InterruptedException e) {
-            // e.printStackTrace();
-            // }
-
-            // TCPClient client = new TCPClient(currNode, neighbourNode);
-            // client.connect();
-
-            // currNode.addNeighbourClient(client);
-            // }
-            // };
-            // Thread cliThread = new Thread(cliRunnable);
-            // cliThread.start();
-            // }
-            // }
-
-            // while (!currNode.areAllNeighboursOnline()) {
-            // try {
-            // Thread.sleep(10000);
-            // } catch (InterruptedException e) {
-            // e.printStackTrace();
-            // }
-
-            // System.out.println("Waiting for all neighbours to come online");
-            // }
+            while (!currNode.areAllNeighboursOnline()) {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("Connected to all neighbours");
 
             // new PelegsLeaderElection(server).startElection();
 
